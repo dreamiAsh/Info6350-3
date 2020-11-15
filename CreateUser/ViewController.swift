714873c3843cab67a3547fe06195007443a0117e
dreamiAsh <ashlyn.leng@gmail.com>
2020-11-14 19:40:03 -0800

    Initial Commit

---
+++ CreateUser/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CreateUser
+//
+//  Created by Leng on 11/14/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
